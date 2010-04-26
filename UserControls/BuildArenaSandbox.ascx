4dd596686a113849cfe58a8875c0f61651e22a37
Daniel Hazelbaker <cabal95@users.noreply.github.com>
2010-04-26 21:17:37 +0000

    Umm, basically everything needed to make the module actually work. A few things are still hard coded, but this isn't likely to be used on any site other than Refresh Cache.

--- UserControls/BuildArenaSandbox.ascx
+++ UserControls/BuildArenaSandbox.ascx
@@ -1,9 +1,20 @@
 <%@ Control Language="C#" AutoEventWireup="true" CodeFile="BuildArenaSandbox.ascx.cs" Inherits="ArenaWeb.UserControls.Custom.HDC.MiscModules.BuildArenaSandbox" %>
 <%@ Register TagPrefix="Arena" Namespace="Arena.Portal.UI" Assembly="Arena.Portal.UI" %>
 
-<pre>
-<asp:Literal ID="lbStatus" runat="server"></asp:Literal>
-</pre>
+<asp:Panel ID="pnlPassword" runat="server" Visible="false">
+  <center><div style="background-color: #ffbb33; border: solid 2px black; width: 450px; padding: 8px; margin-bottom: 20px;">
+  Unable to authenticate you with the password provided. Please try your password again.
+  </span></div>
+</asp:Panel>
 
-Please enter your password: <asp:TextBox ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox><br />
-<asp:Button ID="btnBuild" runat="server" OnClick="btnBuild_Click" />
+<asp:Panel ID="pnlReady" runat="server" Visible="false">
+  <center><div style="background-color: #66bb33; border: solid 2px black; width: 450px; padding: 8px; margin-bottom: 20px;">
+  <asp:Literal ID="ltReady" runat="server"></asp:Literal>
+  </span></div>
+</asp:Panel>
+
+Enter your password: <asp:TextBox ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox><br />
+Select Arena version: <asp:DropDownList ID="ddlVersion" runat="server"></asp:DropDownList> <br />
+<asp:Button ID="btnBuild" runat="server" OnClick="btnBuild_Click" Text="Build" /><br />
+
+<small>(Only click Build once, it can take up to a minute to build your sandbox.)</small>
--- UserControls/BuildArenaSandbox.ascx.cs
+++ UserControls/BuildArenaSandbox.ascx.cs
@@ -9,6 +9,7 @@ namespace ArenaWeb.UserControls.Custom.HDC.MiscModules
 	using System;
 	using System.Data;
     using System.Data.SqlClient;
+    using System.DirectoryServices;
 	using System.Configuration;
 	using System.Collections;
 	using System.Collections.Generic;
@@ -26,9 +27,18 @@ namespace ArenaWeb.UserControls.Custom.HDC.MiscModules
 
 	using Arena.Portal;
 	using Arena.Core;
+    using Arena.Security;
 
 	public partial class BuildArenaSandbox : PortalControl
     {
+        #region Module Settings
+
+        public String AuthenticationDomain { get { return "REFRESHCACHE"; } }
+        public String SandboxPath { get { return "C:\\Arena\\Sandboxes"; } }
+
+        #endregion
+
+
         #region DLL Imports
 
         [DllImport("advapi32.DLL", SetLastError = true)]
@@ -44,87 +54,257 @@ namespace ArenaWeb.UserControls.Custom.HDC.MiscModules
 
         #endregion
 
+
         #region Event Handlers
 
         private void Page_Load(object sender, System.EventArgs e)
 		{
-			if ( ! IsPostBack )
+			if (!IsPostBack)
 			{
-				
+                DirectoryInfo dir = new DirectoryInfo(SandboxPath + "\\Templates");
+
+
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    if (file.Extension.Equals(".BAK", StringComparison.CurrentCultureIgnoreCase) == true)
+                        ddlVersion.Items.Add(file.Name.Substring(0, file.Name.Length - 4));
+                }
 			}
 		}
 
         public void btnBuild_Click(object sender, EventArgs e)
         {
-            SqlConnection sql = null;
-            SqlCommand sqlCommand;
-            SqlDataReader rdr;
-            String userName = "daniel";
-            String password = txtPassword.Text;
-            String database = "ArenaSandbox_" + userName;
-            IntPtr admin_token;
-            int result = 0;
+            pnlPassword.Visible = false;
+            pnlReady.Visible = false;
+            if (String.IsNullOrEmpty(ddlVersion.SelectedValue) == false)
+            {
+                if (CreateSandboxFolder() == false ||
+                    CreateIISApplication() == false ||
+                    CreateSandboxDatabase() == false)
+                {
+                    pnlPassword.Visible = true;
+                }
 
+                String username = ArenaContext.Current.User.Identity.Name;
+                ltReady.Text = "Your sandbox is ready for use at <a href=\"http://arena.refreshcache.com/Sandbox/" + username + "\">http://arena.refreshcache.com/Sandbox/" + username + "</a>. You may login with your same username and password.";
+                pnlReady.Visible = true;
+            }
+        }
+
+        #endregion
+
+        private bool CreateSandboxFolder()
+        {
+            String username = ArenaContext.Current.User.Identity.Name;
+            IntPtr admin_token;
+
+            
+            //
+            // Impersonate the logged in user.
+            //
             try
             {
-                if ((result = LogonUser(userName, "CONSTANTINE", password, 2, 0, out admin_token)) != 0)
+                if (LogonUser(username, AuthenticationDomain, txtPassword.Text, 2, 0, out admin_token) != 0)
                 {
                     ImpersonateLoggedOnUser(admin_token);
 
                     //
                     // Copy the sandbox arena folder to the user's sandbox.
                     //
-                    System.IO.DirectoryInfo src, dst;
-                    src = new System.IO.DirectoryInfo("C:\\Users\\daniel\\Source");
-                    dst = new System.IO.DirectoryInfo("C:\\Users\\daniel\\Dest");
+                    DirectoryInfo src, dst;
+                    src = new DirectoryInfo(SandboxPath + "\\Templates\\" + ddlVersion.SelectedValue);
+                    dst = new DirectoryInfo(SandboxPath + "\\" + username);
+                    try
+                    {
+                        dst.Delete(true);
+                    }
+                    catch { }
                     CopyAll(src, dst);
 
                     //
                     // Modify the web.config to connect to the correct database.
                     //
+                    StreamReader configRdr = new StreamReader(SandboxPath + "\\" + username + "\\web.config");
+                    String config = configRdr.ReadToEnd();
+                    configRdr.Close();
+                    int start = config.IndexOf("Initial Catalog=");
+                    int end = config.IndexOf(";", start);
+                    config = config.Remove(start, (end - start));
+                    config = config.Insert(start, "Initial Catalog=ArenaSandbox_" + username);
+                    config = config.Replace("Templates\\" + ddlVersion.SelectedValue, username);
+                    config = config.Replace("Templates/" + ddlVersion.SelectedValue, username);
+                    StreamWriter configWrt = new StreamWriter(SandboxPath + "\\" + username + "\\web.config");
+                    configWrt.Write(config);
+                    configWrt.Close();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                RevertToSelf();
+                throw ex;
+            }
+
+            //
+            // Cleanup after pretending to be the user.
+            //
+            RevertToSelf();
+
+            return true;
+        }
+
+
+        private bool CreateSandboxDatabase()
+        {
+            SqlConnection sql = null;
+            SqlCommand sqlCommand;
+            SqlDataReader rdr;
+            String username = ArenaContext.Current.User.Identity.Name;
+            String database = "ArenaSandbox_" + username;
+            String dbVersion = ddlVersion.SelectedValue.Replace('.', '_');
+            IntPtr admin_token;
+
+
+            //
+            // Impersonate the logged in user.
+            //
+            try
+            {
+                if (LogonUser(username, AuthenticationDomain, txtPassword.Text, 2, 0, out admin_token) != 0)
+                {
+                    ImpersonateLoggedOnUser(admin_token);
 
                     //
                     // Open a connection to the SQL server.
                     //
-                    sql = new SqlConnection("Data Source=constantine\\hdcarena;Trusted_Connection=yes");
+                    sql = new SqlConnection("Data Source=refreshcacheare\\arena;Trusted_Connection=yes");
                     sql.Open();
 
                     //
-                    // Run a test command.
+                    // Drop and re-create the database.
                     //
-                    sqlCommand = new SqlCommand("SELECT SYSTEM_USER", sql);
-                    rdr = sqlCommand.ExecuteReader();
-                    rdr.Read();
-                    lbStatus.Text += "Connected to SQL as " + rdr[0].ToString() + "\n";
-                    rdr.Close();
+                    try
+                    {
+                        sqlCommand = new SqlCommand("DROP DATABASE " + database, sql);
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    catch { }
+                    sqlCommand = new SqlCommand("RESTORE DATABASE " + database +
+                        " FROM DISK = '" + SandboxPath + "\\Templates\\" + ddlVersion.SelectedValue + ".bak'" +
+                        " WITH MOVE 'Template" + dbVersion + "DB' TO '" + SandboxPath + "\\" + username + ".mdf'" +
+                        ", MOVE 'Template" + dbVersion + "DB_Log' TO '" + SandboxPath + "\\" + username + ".ldf'",
+                        sql);
+                    sqlCommand.ExecuteNonQuery();
 
                     //
-                    // Commands that need to be run:
-                    // DROP DATABASE [database]
-                    // CREATE DATABASE [database]
-                    // ** Assign permissions to the Arena user **
-                    // ** Restore database from snapshot **
-                    // Create user record / login for the current Arena user in new sandbox.
-                    // Assign database permissions to the Arena user.
+                    // Create the current users login as the Administrator of
+                    // the new database.
                     //
+                    Arena.DataLayer.Organization.OrganizationData org = new Arena.DataLayer.Organization.OrganizationData();
+                    rdr = org.ExecuteReader("SELECT [password] FROM secu_login WHERE login_id = '" + username + "'");
+                    rdr.Read();
+                    sql.ChangeDatabase(database);
+                    sqlCommand = new SqlCommand("secu_sp_save_login", sql);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.Add(new SqlParameter("@PersonID", 1));
+                    sqlCommand.Parameters.Add(new SqlParameter("@LoginID", username));
+                    sqlCommand.Parameters.Add(new SqlParameter("@Password", rdr[0]));
+                    sqlCommand.Parameters.Add(new SqlParameter("@UserID", "Sandbox"));
+                    sqlCommand.Parameters.Add(new SqlParameter("@Active", true));
+                    sqlCommand.Parameters.Add(new SqlParameter("@AuthenticationProvider", AuthenticationProvider.Database));
+                    sqlCommand.Parameters.Add(new SqlParameter("@AccountLocked", false));
+                    sqlCommand.Parameters.Add(new SqlParameter("@DateLockExpires", DateTime.Parse("1900-01-01 00:00:00")));
+                    sqlCommand.Parameters.Add(new SqlParameter("@ForceChangePassword", false));
+                    sqlCommand.ExecuteNonQuery();
+
+                    //
+                    // Close the database.
+                    //
+                    sql.Close();
                 }
                 else
-                    lbStatus.Text += "Fail to logon errorcode = " + GetLastError().ToString();
+                {
+                    return false;
+                }
             }
             catch (Exception ex)
-            {
-                lbStatus.Text += ex.Message;
-            }
-            finally
             {
                 if (sql != null)
                     sql.Close();
                 RevertToSelf();
+                throw ex;
             }
+
+            //
+            // Cleanup after pretending to be the user.
+            //
+            RevertToSelf();
+
+            return true;
         }
 
-        #endregion
 
+        private bool CreateIISApplication()
+        {
+            String username = ArenaContext.Current.User.Identity.Name;
+            IntPtr admin_token;
+
+
+            //
+            // Impersonate the administrator user now.
+            //
+            try
+            {
+                Credentials creds = new Credentials(CurrentOrganization.OrganizationID, CredentialType.ActiveDirectory);
+                DirectoryEntry w3svc, sandbox;
+
+                if (LogonUser(creds.Username, AuthenticationDomain, creds.Password, 2, 0, out admin_token) != 0)
+                {
+                    ImpersonateLoggedOnUser(admin_token);
+
+                    //
+                    // Delete the old entry if it exists.
+                    //
+                    if (DirectoryEntry.Exists("IIS://localhost/W3SVC/1/Root/Sandbox/" + username))
+                    {
+                        sandbox = new DirectoryEntry("IIS://localhost/W3SVC/1/Root/Sandbox/cabal95");
+                        sandbox.DeleteTree();
+                    }
+
+                    //
+                    // Create the new directory entry.
+                    //
+                    w3svc = new DirectoryEntry("IIS://localhost/W3SVC/1/Root/Sandbox");
+                    sandbox = w3svc.Children.Add(username, w3svc.SchemaClassName.Replace("Service", "VirtualDir"));
+                    sandbox.Properties["Path"][0] = SandboxPath + "\\" + username;
+                    sandbox.Properties["AccessScript"][0] = true;
+                    sandbox.Properties["AppFriendlyName"][0] = username;
+                    sandbox.Properties["AppIsolated"][0] = "1";
+                    sandbox.Properties["AppRoot"][0] = "/LM/W3SVC/1/Root/Sandbox/" + username;
+                    sandbox.CommitChanges();
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                RevertToSelf();
+                throw ex;
+            }
+
+            //
+            // Cleanup after pretending to be the administrator.
+            //
+            RevertToSelf();
+
+            return true;
+        }
 
         #region Directory manipulation.
 
@@ -139,15 +319,13 @@ namespace ArenaWeb.UserControls.Custom.HDC.MiscModules
             // Copy each file into it's new directory.
             foreach (FileInfo fi in source.GetFiles())
             {
-                lbStatus.Text += String.Format("Copying {0}\\{1}", target.FullName, fi.Name);
                 fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
             }
 
             // Copy each subdirectory using recursion.
             foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
             {
-                DirectoryInfo nextTargetSubDir =
-                    target.CreateSubdirectory(diSourceSubDir.Name);
+                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
                 CopyAll(diSourceSubDir, nextTargetSubDir);
             }
         }
