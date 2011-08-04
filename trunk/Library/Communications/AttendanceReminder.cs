cf21c90385a6ec56105055d304f739a4de3c85f8
cabal95 <cabal95@95079e53-9366-4ecf-bcb3-66278ba757ea>
2011-08-04 23:02:05 +0000

    Add new AttendanceReminder class to handle e-mail interface to database. Add most of the code required to perform the small group attendance reminder agent.

---
+++ trunk/Library/Communications/AttendanceReminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Arena.Core.Communications;
+
+namespace Arena.Custom.HDC.MiscModules.Communications
+{
+    public class AttendanceReminder : CommunicationType
+    {
+        public override string[] GetMergeFields()
+        {
+            List<string> fields = new List<string>();
+
+
+            base.AddPersonMergeFields(fields);
+            fields.Sort();
+
+            return fields.ToArray();
+        }
+    }
+}
