01a510ebeaf8046971e871013043d9bd044a5edd
cabal95 <cabal95@95079e53-9366-4ecf-bcb3-66278ba757ea>
2011-08-19 21:08:06 +0000

    Update the way merge fields are used to not cause issues.

--- trunk/Library/Communications/AttendanceReminder.cs
+++ trunk/Library/Communications/AttendanceReminder.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using Arena.Core;
 using Arena.Core.Communications;
+using Arena.SmallGroup;
 
 namespace Arena.Custom.HDC.MiscModules.Communications
 {
@@ -20,10 +22,35 @@ namespace Arena.Custom.HDC.MiscModules.Communications
             List<string> fields = new List<string>();
 
 
-            base.AddPersonMergeFields(fields);
+            fields.Add("##FirstName##");
+            fields.Add("##FormalName##");
+            fields.Add("##FullName##");
+            fields.Add("##LastName##");
+            fields.Add("##MiddleName##");
+            fields.Add("##NickName##");
+            fields.Add("##Suffix##");
+            fields.Add("##Title##");
+            fields.Add("##GroupName##");
+            fields.Add("##GroupID##");
+
             fields.Sort();
 
             return fields.ToArray();
+        }
+
+
+        public void LoadFields(Dictionary<string, string> fields, Person person, Group group)
+        {
+            fields.Add("##FirstName##", person.FirstName);
+            fields.Add("##FormalName##", person.FormalName);
+            fields.Add("##FullName##", person.FullName);
+            fields.Add("##LastName##", person.LastName);
+            fields.Add("##MiddleName##", person.MiddleName);
+            fields.Add("##NickName##", person.NickName);
+            fields.Add("##Suffix##", person.Suffix.Value);
+            fields.Add("##Title##", person.Title.Value);
+            fields.Add("##GroupName##", group.Name);
+            fields.Add("##GroupID##", group.GroupID.ToString());
         }
     }
 }
